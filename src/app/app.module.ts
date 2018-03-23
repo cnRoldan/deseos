7e86c4250f1014d3014eba9e31212ad8bbcd74ab
Roldan Ramos <claudio-nicolas.roldanramos.external@atos.net>
2018-03-23 13:07:05 +0100

    Cambio de colores y añado cards

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,21 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 
-import { AboutPage } from '../pages/about/about';
-import { ContactPage } from '../pages/contact/contact';
-import { HomePage } from '../pages/home/home';
+//Servicios
+import {  ListaDeseosService  } from './services/lista-deseos.services';
+
 import { TabsPage } from '../pages/tabs/tabs';
+import {  PendientesComponent } from '../pages/pendientes/pendientes.component';
+import { TerminadosComponent } from '../pages/terminados/terminados.component';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
+
 @NgModule({
   declarations: [
     MyApp,
-    AboutPage,
-    ContactPage,
-    HomePage,
-    TabsPage
+    TabsPage,
+    PendientesComponent,
+    TerminadosComponent
   ],
   imports: [
     BrowserModule,
@@ -26,14 +28,14 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    AboutPage,
-    ContactPage,
-    HomePage,
-    TabsPage
+    TabsPage,
+    PendientesComponent,
+    TerminadosComponent
   ],
   providers: [
     StatusBar,
     SplashScreen,
+    ListaDeseosService,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
