b2e84aa9fe9227464acd4a25c44c93329c8abde6
Roldan Ramos <claudio-nicolas.roldanramos.external@atos.net>
2018-03-25 22:23:40 +0200

    Commit hasta la practica 106

--- src/app/services/lista-deseos.services.ts
+++ src/app/services/lista-deseos.services.ts
@@ -9,21 +9,22 @@ export class ListaDeseosService {
 
   constructor() {
 
-    let lista1 = new Lista('Compras de supermercado');
-    let lista2 = new Lista('Juegos de deseo');
-    let lista3 = new Lista('Cosas de la universidad');
-
-    try {
-      if (lista1!=null&&lista2!=null&&lista3!=null){
-        this.listas.push(lista1);
-        this.listas.push(lista2);
-        this.listas.push(lista3);
-      }
-    }catch(e){
-
-    }
-
-
+    this.cargarData();
     console.log("Servicio creado");
+   }
+
+  actualizarData(){
+     localStorage.setItem("data", JSON.stringify(this.listas));
+   }
+
+   cargarData(){
+     if ( localStorage.getItem("data"))
+      this.listas = JSON.parse(localStorage.getItem("data"));
+   }
+
+   agregarLista(lista:Lista){
+     this.listas.push(lista);
+     this.actualizarData();
+
    }
 }
--- src/pages/agregar/agregar.component.ts
+++ src/pages/agregar/agregar.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {Lista,ListaItem} from '../../app/clases/index';
+import { AlertController, NavController} from 'ionic-angular';
+import { ListaDeseosService } from '../../app/services/lista-deseos.services'
 
 @Component({
   selector: 'app-agregar',
@@ -6,10 +9,53 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AgregarComponent implements OnInit {
 
-  nombreLista:string;
-  nombreItem:string;
+  nombreLista:string="";
+  nombreItem:string="";
 
-  constructor() {  }
+  items:ListaItem[] = [];
 
-  ngOnInit() {}
+  constructor(
+    public alertCtrl:AlertController,
+    public navCtrl:NavController,
+    public _listaDeseos:ListaDeseosService
+  ) {  }
+
+  ngOnInit() {
+
+  }
+
+  agregar(){
+    if (this.nombreItem.length == 0){
+      return
+    }
+
+
+    let item = new ListaItem();
+    item.nombre = this.nombreItem;
+
+    this.items.push(item);
+    this.nombreItem = "";
+  }
+
+  eliminar(index:number){
+    this.items.splice(index,1);
+  }
+
+  guardarLista(){
+    if ( this.nombreLista.length==0){
+      let alert = this.alertCtrl.create({
+        title: 'Nombre de la lista',
+        subTitle: 'La lista necesita un nombre',
+        buttons: ['OK']
+      });
+      alert.present();
+      return;
+    }
+
+    let lista = new Lista( this.nombreLista);
+    lista.items = this.items;
+
+    this._listaDeseos.agregarLista(lista);
+    this.navCtrl.pop();
+  }
 }
